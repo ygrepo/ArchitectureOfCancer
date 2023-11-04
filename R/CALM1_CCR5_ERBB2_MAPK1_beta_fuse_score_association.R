709f210de20a12de4dcca678939342be8d0b037b
Yves Greatti <ygrepo@gmail.com>
2023-11-04 18:13:43 -0400

    Added ClinVar Annotations

--- R/CALM1_CCR5_ERBB2_MAPK1_beta_fuse_score_association.R
+++ R/CALM1_CCR5_ERBB2_MAPK1_beta_fuse_score_association.R
@@ -33,12 +33,12 @@ mave_data %>%
   unique() %>%
   print_html_df(caption_txt = "Clinvar Significance, Categories And Label")
 
-# MSH2, Colon Cancer ----
+# CALM1, RCC Cancer ----
 
-gene_target <- "MSH2"
+gene_target <- "CALM1"
 
 cancer_filenames <- getCancerFiles("data/t0.01", gene_target)
-data <- as_tibble(read.table(cancer_filenames[["ColonCancer"]],
+data <- as_tibble(read.table(cancer_filenames[["KidneyRenalCellCancer"]],
   header = TRUE, sep = ","
 )) %>%
   mutate(ProteinChange = str_extract(hgvsp, "(?<=:).+"))
@@ -62,9 +62,9 @@ unique(data$id)
 # Compute correlation and p-value
 studies.data <- getDataByStudy(data)
 
-# MSH2, Colon Cancer, 00000003-a-2 ----
+# CALM1, RCC Cancer, 00000001-c-1 ----
 # Compute correlation and p-value
-study <- "urn:mavedb:00000050-a-1"
+study <- "urn:mavedb:00000001-c-1"
 study.data <- studies.data$Data[studies.data$Study == study][[1]]
 res_corr <- get_beta_FUSE_correlation_p_value(study.data)
 
@@ -72,7 +72,7 @@ res_corr <- get_beta_FUSE_correlation_p_value(study.data)
 study_abb <- gsub("urn:mavedb:", "", study)
 study_abb
 title_txt <- bquote(paste(
-  "MSH2, Colon Cancer, study=", .(study_abb), ", ", beta, " vs. FUSE Score, ", rho, "=",
+  "CALM1, RCC Cancer, study=", .(study_abb), ", ", beta, " vs. FUSE Score, ", rho, "=",
   .(round(res_corr$corr, 2)), " , p=", .(round(res_corr$pval, 4))
 ))
 
@@ -99,27 +99,27 @@ pt <- getScatterPlot(study.data,
   x_y_font_size = 12,
   annotate_text_size = 2,
   annotate.point = TRUE,
-  xlimits = xlimits,
-  xbreaks = xbreaks,
-  ylimits = ylimits,
-  ybreaks = ybreaks
-  # xlimits = NULL,
-  # xbreaks = NULL,
-  # ylimits = NULL,
-  # ybreaks = NULL
+  # xlimits = xlimits,
+  # xbreaks = xbreaks,
+  # ylimits = ylimits,
+  # ybreaks = ybreaks
+  xlimits = NULL,
+  xbreaks = NULL,
+  ylimits = NULL,
+  ybreaks = NULL
   
 )
 pt
-filename <- "20231104_MSH2_Colon_00000050-a-1_beta_fuse.png"
+filename <- "20231104_CALM1_RCC_00000001-c-1_beta_fuse.png"
 file_path <- paste0(figure_path, filename)
 graph2png(pt, file = file_path, dpi = 600, aspectr = 1.2)
 
 
-# MSH2, Colorectal Cancer ----
+# CCR5, FH Breast Cancer ----
 
-gene_target <- "MSH2"
-
-data <- as_tibble(read.table(cancer_filenames[["ColorectalCancer"]],
+gene_target <- "CCR5"
+cancer_filenames <- getCancerFiles("data/t0.01", gene_target)
+data <- as_tibble(read.table(cancer_filenames[["FHBreastCancer"]],
                              header = TRUE, sep = ","
 )) %>%
   mutate(ProteinChange = str_extract(hgvsp, "(?<=:).+"))
@@ -133,7 +133,8 @@ common_variants <- intersect(
 print(length(unique(common_variants)))
 
 data <- data %>%
-  inner_join(mave_data %>% filter(gene == gene_target), by = "ProteinChange") %>%
+  inner_join(mave_data %>% filter(gene == gene_target), 
+             by = "ProteinChange", relationship = "many-to-many") %>%
   mutate(ClinVarLabelP = if_else(ClinVarLabel == "LP/P", ProteinChange, "")) %>%
   mutate(ClinVarLabelP = gsub("p.", "", ClinVarLabelP))
 
@@ -143,9 +144,9 @@ unique(data$id)
 # Compute correlation and p-value
 studies.data <- getDataByStudy(data)
 
-# MSH2, Colorectal Cancer, 00000003-a-2 ----
+# CCR5, FH Breast Cancer, 00000047-a-1 ----
 # Compute correlation and p-value
-study <- "urn:mavedb:00000050-a-1"
+study <- "urn:mavedb:00000047-a-1"
 study.data <- studies.data$Data[studies.data$Study == study][[1]]
 res_corr <- get_beta_FUSE_correlation_p_value(study.data)
 
@@ -153,7 +154,7 @@ res_corr <- get_beta_FUSE_correlation_p_value(study.data)
 study_abb <- gsub("urn:mavedb:", "", study)
 study_abb
 title_txt <- bquote(paste(
-  "MSH2, Colorectal Cancer, study=", .(study_abb), ", ", beta, " vs. FUSE Score, ", rho, "=",
+  "CCR5, FH Breast Cancer, study=", .(study_abb), ", ", beta, " vs. FUSE Score, ", rho, "=",
   .(round(res_corr$corr, 2)), " , p=", .(round(res_corr$pval, 4))
 ))
 
@@ -162,10 +163,10 @@ ylabel <- "Beta"
 x_col <- "FUSE_score"
 y_col <- "beta"
 
-xlimits <- c(-3, 2)
-xbreaks <- seq(-3, 2, by = 1)
-ylimits <- c(-1.1, 11)
-ybreaks <- seq(-1.1, 11, by = 1)
+xlimits <- c(-2, 1)
+xbreaks <- seq(-2, 1, by = 1)
+ylimits <- c(-1.1, 5)
+ybreaks <- seq(-1.1, 5, by = 1)
 
 pt <- getScatterPlot(study.data,
                      title_txt,
@@ -191,42 +192,13 @@ pt <- getScatterPlot(study.data,
                      
 )
 pt
-filename <- "20231104_MSH2_Colorectal_00000050-a-1_beta_fuse.png"
+filename <- "20231104_CCR5_FHBC_00000047-a-1_beta_fuse.png"
 file_path <- paste0(figure_path, filename)
 graph2png(pt, file = file_path, dpi = 600, aspectr = 1.2)
 
-
-# MSH2, FHBowel Cancer ----
-
-gene_target <- "MSH2"
-
-data <- as_tibble(read.table(cancer_filenames[["FHBowelCancer"]],
-                             header = TRUE, sep = ","
-)) %>%
-  mutate(ProteinChange = str_extract(hgvsp, "(?<=:).+"))
-
-
-common_variants <- intersect(
-  unique(data$ProteinChange),
-  unique(mave_data$ProteinChange)
-)
-
-print(length(unique(common_variants)))
-
-data <- data %>%
-  inner_join(mave_data %>% filter(gene == gene_target), by = "ProteinChange") %>%
-  mutate(ClinVarLabelP = if_else(ClinVarLabel == "LP/P", ProteinChange, "")) %>%
-  mutate(ClinVarLabelP = gsub("p.", "", ClinVarLabelP))
-
-
-unique(data$id)
-
+# CCR5, FH Breast Cancer, 00000047-b-1 ----
 # Compute correlation and p-value
-studies.data <- getDataByStudy(data)
-
-# MSH2, FHBowel Cancer, 00000003-a-2 ----
-# Compute correlation and p-value
-study <- "urn:mavedb:00000050-a-1"
+study <- "urn:mavedb:00000047-b-1"
 study.data <- studies.data$Data[studies.data$Study == study][[1]]
 res_corr <- get_beta_FUSE_correlation_p_value(study.data)
 
@@ -234,7 +206,7 @@ res_corr <- get_beta_FUSE_correlation_p_value(study.data)
 study_abb <- gsub("urn:mavedb:", "", study)
 study_abb
 title_txt <- bquote(paste(
-  "MSH2, FH Bowel Cancer, study=", .(study_abb), ", ", beta, " vs. FUSE Score, ", rho, "=",
+  "CCR5, FH Breast Cancer, study=", .(study_abb), ", ", beta, " vs. FUSE Score, ", rho, "=",
   .(round(res_corr$corr, 2)), " , p=", .(round(res_corr$pval, 4))
 ))
 
@@ -243,10 +215,10 @@ ylabel <- "Beta"
 x_col <- "FUSE_score"
 y_col <- "beta"
 
-xlimits <- c(-3, 2)
-xbreaks <- seq(-3, 2, by = 1)
-ylimits <- c(-1.1, 11)
-ybreaks <- seq(-1.1, 11, by = 1)
+xlimits <- c(-2, 1)
+xbreaks <- seq(-2, 1, by = 1)
+ylimits <- c(-1.1, 5)
+ybreaks <- seq(-1.1, 5, by = 1)
 
 pt <- getScatterPlot(study.data,
                      title_txt,
@@ -272,41 +244,13 @@ pt <- getScatterPlot(study.data,
                      
 )
 pt
-filename <- "20231104_MSH2_FHBowel_00000050-a-1_beta_fuse.png"
+filename <- "20231104_CCR5_FHBC_00000047-b-1_beta_fuse.png"
 file_path <- paste0(figure_path, filename)
 graph2png(pt, file = file_path, dpi = 600, aspectr = 1.2)
 
-# MSH2, LargeBowel Cancer ----
-
-gene_target <- "MSH2"
-
-data <- as_tibble(read.table(cancer_filenames[["LargeBowelCancer"]],
-                             header = TRUE, sep = ","
-)) %>%
-  mutate(ProteinChange = str_extract(hgvsp, "(?<=:).+"))
-
-
-common_variants <- intersect(
-  unique(data$ProteinChange),
-  unique(mave_data$ProteinChange)
-)
-
-print(length(unique(common_variants)))
-
-data <- data %>%
-  inner_join(mave_data %>% filter(gene == gene_target), by = "ProteinChange") %>%
-  mutate(ClinVarLabelP = if_else(ClinVarLabel == "LP/P", ProteinChange, "")) %>%
-  mutate(ClinVarLabelP = gsub("p.", "", ClinVarLabelP))
-
-
-unique(data$id)
-
+# CCR5, FH Breast Cancer, 00000047-c-1 ----
 # Compute correlation and p-value
-studies.data <- getDataByStudy(data)
-
-# MSH2, LargeBowel Cancer, 00000003-a-2 ----
-# Compute correlation and p-value
-study <- "urn:mavedb:00000050-a-1"
+study <- "urn:mavedb:00000047-c-1"
 study.data <- studies.data$Data[studies.data$Study == study][[1]]
 res_corr <- get_beta_FUSE_correlation_p_value(study.data)
 
@@ -314,7 +258,7 @@ res_corr <- get_beta_FUSE_correlation_p_value(study.data)
 study_abb <- gsub("urn:mavedb:", "", study)
 study_abb
 title_txt <- bquote(paste(
-  "MSH2, Large Bowel Cancer, study=", .(study_abb), ", ", beta, " vs. FUSE Score, ", rho, "=",
+  "CCR5, FH Breast Cancer, study=", .(study_abb), ", ", beta, " vs. FUSE Score, ", rho, "=",
   .(round(res_corr$corr, 2)), " , p=", .(round(res_corr$pval, 4))
 ))
 
@@ -323,10 +267,10 @@ ylabel <- "Beta"
 x_col <- "FUSE_score"
 y_col <- "beta"
 
-xlimits <- c(-3, 2)
-xbreaks <- seq(-3, 2, by = 1)
-ylimits <- c(-1.1, 11)
-ybreaks <- seq(-1.1, 11, by = 1)
+xlimits <- c(-2, 1)
+xbreaks <- seq(-2, 1, by = 1)
+ylimits <- c(-1.1, 5)
+ybreaks <- seq(-1.1, 5, by = 1)
 
 pt <- getScatterPlot(study.data,
                      title_txt,
@@ -352,15 +296,14 @@ pt <- getScatterPlot(study.data,
                      
 )
 pt
-filename <- "20231104_MSH2_LargeBowel_00000050-a-1_beta_fuse.png"
+filename <- "20231104_CCR5_FHBC_00000047-c-1_beta_fuse.png"
 file_path <- paste0(figure_path, filename)
 graph2png(pt, file = file_path, dpi = 600, aspectr = 1.2)
 
+# ERBB2, Rectal Cancer ----
 
-# MSH2, Rectal Cancer ----
-
-gene_target <- "MSH2"
-
+gene_target <- "ERBB2"
+cancer_filenames <- getCancerFiles("data/t0.01", gene_target)
 data <- as_tibble(read.table(cancer_filenames[["RectalCancer"]],
                              header = TRUE, sep = ","
 )) %>%
@@ -375,7 +318,8 @@ common_variants <- intersect(
 print(length(unique(common_variants)))
 
 data <- data %>%
-  inner_join(mave_data %>% filter(gene == gene_target), by = "ProteinChange") %>%
+  inner_join(mave_data %>% filter(gene == gene_target), 
+             by = "ProteinChange", relationship = "many-to-many") %>%
   mutate(ClinVarLabelP = if_else(ClinVarLabel == "LP/P", ProteinChange, "")) %>%
   mutate(ClinVarLabelP = gsub("p.", "", ClinVarLabelP))
 
@@ -385,9 +329,9 @@ unique(data$id)
 # Compute correlation and p-value
 studies.data <- getDataByStudy(data)
 
-# MSH2, Rectal Cancer, 00000003-a-2 ----
+# ERBB2, Rectal Cancer, 00000051-b-1" ----
 # Compute correlation and p-value
-study <- "urn:mavedb:00000050-a-1"
+study <- "urn:mavedb:00000051-b-1"
 study.data <- studies.data$Data[studies.data$Study == study][[1]]
 res_corr <- get_beta_FUSE_correlation_p_value(study.data)
 
@@ -395,7 +339,7 @@ res_corr <- get_beta_FUSE_correlation_p_value(study.data)
 study_abb <- gsub("urn:mavedb:", "", study)
 study_abb
 title_txt <- bquote(paste(
-  "MSH2, Rectal Cancer, study=", .(study_abb), ", ", beta, " vs. FUSE Score, ", rho, "=",
+  "ERBB2, Rectal Cancer, study=", .(study_abb), ", ", beta, " vs. FUSE Score, ", rho, "=",
   .(round(res_corr$corr, 2)), " , p=", .(round(res_corr$pval, 4))
 ))
 
@@ -404,10 +348,10 @@ ylabel <- "Beta"
 x_col <- "FUSE_score"
 y_col <- "beta"
 
-xlimits <- c(-3, 2)
-xbreaks <- seq(-3, 2, by = 1)
-ylimits <- c(-1.1, 11)
-ybreaks <- seq(-1.1, 11, by = 1)
+xlimits <- c(-2, 1)
+xbreaks <- seq(-2, 1, by = 1)
+ylimits <- c(-1.1, 5)
+ybreaks <- seq(-1.1, 5, by = 1)
 
 pt <- getScatterPlot(study.data,
                      title_txt,
@@ -422,26 +366,28 @@ pt <- getScatterPlot(study.data,
                      x_y_font_size = 12,
                      annotate_text_size = 2,
                      annotate.point = TRUE,
-                     xlimits = xlimits,
-                     xbreaks = xbreaks,
-                     ylimits = ylimits,
-                     ybreaks = ybreaks
-                     # xlimits = NULL,
-                     # xbreaks = NULL,
-                     # ylimits = NULL,
-                     # ybreaks = NULL
+                     # xlimits = xlimits,
+                     # xbreaks = xbreaks,
+                     # ylimits = ylimits,
+                     # ybreaks = ybreaks
+                     xlimits = NULL,
+                     xbreaks = NULL,
+                     ylimits = NULL,
+                     ybreaks = NULL
                      
 )
 pt
-filename <- "20231104_MSH2_rectal_00000050-a-1_beta_fuse.png"
+filename <- "20231104_ERBB2_rectal_00000051-b-1_beta_fuse.png"
 file_path <- paste0(figure_path, filename)
 graph2png(pt, file = file_path, dpi = 600, aspectr = 1.2)
 
-# MSH2, Small Intestine Cancer ----
 
-gene_target <- "MSH2"
+# MAPK1, C43 Malignant Skin Cancer ----
 
-data <- as_tibble(read.table(cancer_filenames[["SmallIntestineCancer"]],
+gene_target <- "MAPK1"
+
+cancer_filenames <- getCancerFiles("data/t0.01", gene_target)
+data <- as_tibble(read.table(cancer_filenames[["C43MalignantSkinCancer"]],
                              header = TRUE, sep = ","
 )) %>%
   mutate(ProteinChange = str_extract(hgvsp, "(?<=:).+"))
@@ -465,9 +411,9 @@ unique(data$id)
 # Compute correlation and p-value
 studies.data <- getDataByStudy(data)
 
-# MSH2, Small Intestine Cancer, 00000003-a-2 ----
+# MAPK1, C43 Malignant Skin Cancer, 00000103-b-1 ----
 # Compute correlation and p-value
-study <- "urn:mavedb:00000050-a-1"
+study <- "urn:mavedb:00000103-b-1"
 study.data <- studies.data$Data[studies.data$Study == study][[1]]
 res_corr <- get_beta_FUSE_correlation_p_value(study.data)
 
@@ -475,7 +421,7 @@ res_corr <- get_beta_FUSE_correlation_p_value(study.data)
 study_abb <- gsub("urn:mavedb:", "", study)
 study_abb
 title_txt <- bquote(paste(
-  "MSH2, Small Intestine Cancer, study=", .(study_abb), ", ", beta, " vs. FUSE Score, ", rho, "=",
+  "MAPK1, Skin Cancer, study=", .(study_abb), ", ", beta, " vs. FUSE Score, ", rho, "=",
   .(round(res_corr$corr, 2)), " , p=", .(round(res_corr$pval, 4))
 ))
 
@@ -502,106 +448,25 @@ pt <- getScatterPlot(study.data,
                      x_y_font_size = 12,
                      annotate_text_size = 2,
                      annotate.point = TRUE,
-                     xlimits = xlimits,
-                     xbreaks = xbreaks,
-                     ylimits = ylimits,
-                     ybreaks = ybreaks
-                     # xlimits = NULL,
-                     # xbreaks = NULL,
-                     # ylimits = NULL,
-                     # ybreaks = NULL
+                     # xlimits = xlimits,
+                     # xbreaks = xbreaks,
+                     # ylimits = ylimits,
+                     # ybreaks = ybreaks
+                     xlimits = NULL,
+                     xbreaks = NULL,
+                     ylimits = NULL,
+                     ybreaks = NULL
                      
 )
 pt
-filename <- "20231104_MSH2_small_intestine_00000050-a-1_beta_fuse.png"
+filename <- "20231104_MAPK1_skin_00000103-b-1_beta_fuse.png"
 file_path <- paste0(figure_path, filename)
 graph2png(pt, file = file_path, dpi = 600, aspectr = 1.2)
 
 
-# MSH2,Prostate Cancer ----
+# MAPK1, Squamous Cell Cancer ----
 
-gene_target <- "MSH2"
-
-data <- as_tibble(read.table(cancer_filenames[["ProstateCancer"]],
-                             header = TRUE, sep = ","
-)) %>%
-  mutate(ProteinChange = str_extract(hgvsp, "(?<=:).+"))
-
-
-common_variants <- intersect(
-  unique(data$ProteinChange),
-  unique(mave_data$ProteinChange)
-)
-
-print(length(unique(common_variants)))
-
-data <- data %>%
-  inner_join(mave_data %>% filter(gene == gene_target), by = "ProteinChange") %>%
-  mutate(ClinVarLabelP = if_else(ClinVarLabel == "LP/P", ProteinChange, "")) %>%
-  mutate(ClinVarLabelP = gsub("p.", "", ClinVarLabelP))
-
-
-unique(data$id)
-
-# Compute correlation and p-value
-studies.data <- getDataByStudy(data)
-
-# MSH2,Prostate Cancer, 00000003-a-2 ----
-# Compute correlation and p-value
-study <- "urn:mavedb:00000050-a-1"
-study.data <- studies.data$Data[studies.data$Study == study][[1]]
-res_corr <- get_beta_FUSE_correlation_p_value(study.data)
-
-
-study_abb <- gsub("urn:mavedb:", "", study)
-study_abb
-title_txt <- bquote(paste(
-  "MSH2, Prostate Cancer, study=", .(study_abb), ", ", beta, " vs. FUSE Score, ", rho, "=",
-  .(round(res_corr$corr, 2)), " , p=", .(round(res_corr$pval, 4))
-))
-
-xlabel <- "FUSE Score"
-ylabel <- "Beta"
-x_col <- "FUSE_score"
-y_col <- "beta"
-
-xlimits <- c(-3, 2)
-xbreaks <- seq(-3, 2, by = 1)
-ylimits <- c(-1.1, 11)
-ybreaks <- seq(-1.1, 11, by = 1)
-
-pt <- getScatterPlot(study.data,
-                     title_txt,
-                     xlabel,
-                     ylabel,
-                     x_col,
-                     y_col,
-                     alpha = 3,
-                     point_size = 1,
-                     point_color = "#00AFBB",
-                     title_font_size = 8,
-                     x_y_font_size = 12,
-                     annotate_text_size = 2,
-                     annotate.point = TRUE,
-                     xlimits = xlimits,
-                     xbreaks = xbreaks,
-                     ylimits = ylimits,
-                     ybreaks = ybreaks
-                     # xlimits = NULL,
-                     # xbreaks = NULL,
-                     # ylimits = NULL,
-                     # ybreaks = NULL
-                     
-)
-pt
-filename <- "20231104_MSH2_prostate_00000050-a-1_beta_fuse.png"
-file_path <- paste0(figure_path, filename)
-graph2png(pt, file = file_path, dpi = 600, aspectr = 1.2)
-
-
-# MSH2, Squamous Cell Cancer ----
-
-gene_target <- "MSH2"
+gene_target <- "MAPK1"
 
 data <- as_tibble(read.table(cancer_filenames[["SquamousCellCancer"]],
                              header = TRUE, sep = ","
@@ -627,9 +492,9 @@ unique(data$id)
 # Compute correlation and p-value
 studies.data <- getDataByStudy(data)
 
-# MSH2,  Squamous Cell Cancer, 00000003-a-2 ----
+# MAPK1, Squamous CellCancer, 00000103-b-1 ----
 # Compute correlation and p-value
-study <- "urn:mavedb:00000050-a-1"
+study <- "urn:mavedb:00000103-b-1"
 study.data <- studies.data$Data[studies.data$Study == study][[1]]
 res_corr <- get_beta_FUSE_correlation_p_value(study.data)
 
@@ -637,7 +502,7 @@ res_corr <- get_beta_FUSE_correlation_p_value(study.data)
 study_abb <- gsub("urn:mavedb:", "", study)
 study_abb
 title_txt <- bquote(paste(
-  "MSH2, Squamous Cell Cancer, study=", .(study_abb), ", ", beta, " vs. FUSE Score, ", rho, "=",
+  "MAPK1, Squamous Cell Cancer, study=", .(study_abb), ", ", beta, " vs. FUSE Score, ", rho, "=",
   .(round(res_corr$corr, 2)), " , p=", .(round(res_corr$pval, 4))
 ))
 
@@ -664,18 +529,17 @@ pt <- getScatterPlot(study.data,
                      x_y_font_size = 12,
                      annotate_text_size = 2,
                      annotate.point = TRUE,
-                     xlimits = xlimits,
-                     xbreaks = xbreaks,
-                     ylimits = ylimits,
-                     ybreaks = ybreaks
-                     # xlimits = NULL,
-                     # xbreaks = NULL,
-                     # ylimits = NULL,
-                     # ybreaks = NULL
+                     # xlimits = xlimits,
+                     # xbreaks = xbreaks,
+                     # ylimits = ylimits,
+                     # ybreaks = ybreaks
+                     xlimits = NULL,
+                     xbreaks = NULL,
+                     ylimits = NULL,
+                     ybreaks = NULL
                      
 )
 pt
-filename <- "20231104_MSH2_squamous_cell_00000050-a-1_beta_fuse.png"
+filename <- "20231104_MAPK1_squamous_cell_00000103-b-1_beta_fuse.png"
 file_path <- paste0(figure_path, filename)
 graph2png(pt, file = file_path, dpi = 600, aspectr = 1.2)
-
