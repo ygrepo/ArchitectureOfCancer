38bc20c2c8208c5416044d497819b78230c9b46c
Yves Greatti <ygrepo@gmail.com>
2024-01-21 09:45:09 -0500

    Added more lsf jobs

---
+++ qsubs/run_polyphen_p_categories.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Check if gene list file is provided
+if [ -z "$1" ]; then
+    echo "Usage: $0 [path to gene list file]"
+    exit 1
+fi
+
+# File containing list of genes
+GENE_LIST_FILE=$1
+
+module purge
+module load R/4.2.0
+
+
+# Loop through each line in the file
+while IFS= read -r gene; do
+    echo "Processing gene" $gene
+    Rscript ~/github/ArchitectureOfCancer/R/polyphen_p_categories.R "$gene"
+done < "$GENE_LIST_FILE"
+
